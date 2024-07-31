908c4e385e7df4cf320820c1c0392cc108b564b3
TomKronsbein <tom.kronsbein@d-fine.com>
2024-07-31 11:05:51 +0200

    Chore: Write StorageConfig(defines hostname, port) and StorageClient(defines functions based on grpc calls)

--- lib/data/data_providers/storage_client.dart
+++ lib/data/data_providers/storage_client.dart
@@ -1,24 +1,77 @@
+//import 'dart:js_interop_unsafe';
+
 import 'package:flutter_ics_homescreen/export.dart';
-import 'package:protos/storage-api.dart' as storage_api; //includes storage_api.pbserver unlike radio
+import 'package:protos/storage-api.dart' as storage_api; 
+
 class StorageClient{
-  final RadioConfig config; //StorageConfig no class, RadioConfic part of app_config_provider
+  final StorageConfig config; 
   final Ref ref;
   late storage_api.ClientChannel channel;
-  late storage_api.DatabaseServiceBase stub; // RadioClient extents $grpc.Client in radio.pbgrpc.dart, .pbgrpc.dart
+  late storage_api.DatabaseClient stub; 
 
- StorageClient({required this.config, required this.ref}) { //construcor
+ StorageClient({required this.config, required this.ref}) {
     debugPrint(
         "Connecting to radio service at ${config.hostname}:${config.port}");
     storage_api.ChannelCredentials creds = const storage_api.ChannelCredentials.insecure();
     channel = storage_api.ClientChannel(config.hostname,
         port: config.port, options: storage_api.ChannelOptions(credentials: creds));
-    stub = storage_api.DatabaseServiceBase(channel);
+    stub = storage_api.DatabaseClient(channel);
   }
 
-// TODO: 'DestroyDB'
-// TODO: 'Write'
-// TODO:  'Delete'
-// TODO: 'Search'
-// TODO: 'DeleteNodes'
-// TODO: 'ListNodes'
+
+  Future<void> destroyDB() async {
+    try {
+      var response = await stub.destroyDB(storage_api.DestroyArguments());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+  Future<void> write(storage_api.KeyValue keyValue) async { 
+    try {
+      var response = await stub.write(keyValue);
+    } catch (e) {
+      print(e);
+    }
+  }
+  Future<void> read(storage_api.Key key) async{
+    try{
+      var response = await stub.read(key);
+    } catch(e) {
+      print(e);
+    }
+  } 
+
+  Future<void> delete(storage_api.Key key) async{
+    try{
+      var response = await stub.delete(key);
+    } catch(e) {
+      print(e);
+    }
+  } 
+
+    Future<void> search(storage_api.Key key) async{
+    try{
+      var response = await stub.search(key);
+    } catch(e) {
+      print(e);
+    }
+  } 
+
+    Future<void> deleteNodes(storage_api.Key key) async{
+    try{
+      var response = await stub.deleteNodes(key);
+    } catch(e) {
+      print(e);
+    }
+  } 
+
+    Future<void> listNodes(storage_api.SubtreeInfo subtreeInfo) async{
+    try{
+      var response = await stub.listNodes(subtreeInfo);
+    } catch(e) {
+      print(e);
+    }
+  } 
 }
